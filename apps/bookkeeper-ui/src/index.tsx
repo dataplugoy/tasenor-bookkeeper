b505c6647b5bc57cbadbdd38e050a75a4b6b7352
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2023-07-21 12:56:18 +0300

    Start working on porting from old JSX to quick and dirty TSX.

--- apps/bookkeeper-ui/src/index.tsx
+++ apps/bookkeeper-ui/src/index.tsx
@@ -1,31 +1,31 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import { I18nextProvider } from 'react-i18next'
-import { Provider } from 'mobx-react'
-import App from './App'
-import { BrowserRouter, withRouter } from 'react-router-dom'
-import Store from './Stores/Store'
-import Cursor from './Stores/Cursor'
-import Settings from './Stores/Settings'
-import i18n, { initializeI18n } from './i18n'
-import { CircularProgress, ThemeProvider } from '@mui/material'
-import theme from './theme'
-import Catalog from './Stores/Catalog'
-import ReactRouterPropTypes from 'react-router-prop-types'
-import { RISPProvider } from '@dataplug/tasenor-common-ui'
-import { setGlobalComponents, Knowledge } from '@dataplug/tasenor-common'
-import { Box } from '@mui/system'
+// import { I18nextProvider } from 'react-i18next'
+// import { Provider } from 'mobx-react'
+// import App from './App'
+// import { BrowserRouter, withRouter } from 'react-router-dom'
+// import Store from './Stores/Store'
+// import Cursor from './Stores/Cursor'
+// import Settings from './Stores/Settings'
+// import i18n, { initializeI18n } from './i18n'
+// import { CircularProgress, ThemeProvider } from '@mui/material'
+// import theme from './theme'
+// import Catalog from './Stores/Catalog'
+// import ReactRouterPropTypes from 'react-router-prop-types'
+// import { RISPProvider } from '@dataplug/tasenor-common-ui'
+// import { setGlobalComponents, Knowledge } from '@dataplug/tasenor-common'
+// import { Box } from '@mui/system'
 
-const settings = new Settings()
-const store = new Store(settings)
-const cursor = new Cursor(store)
-const catalog = new Catalog(store)
-store.setCatalog(catalog)
-const knowledge = new Knowledge()
+// const settings = new Settings()
+// const store = new Store(settings)
+// const cursor = new Cursor(store)
+// const catalog = new Catalog(store)
+// store.setCatalog(catalog)
+// const knowledge = new Knowledge()
+//
+// setGlobalComponents(store, catalog, cursor, settings, knowledge)
 
-setGlobalComponents(store, catalog, cursor, settings, knowledge)
-
-@withRouter
+// @withRouter
 class AppRenderer extends Component {
   state = {
     loading: true
@@ -54,10 +54,11 @@ class AppRenderer extends Component {
     return <I18nextProvider i18n={ i18n }><App /></I18nextProvider>
   }
 }
-AppRenderer.propTypes = {
-  history: ReactRouterPropTypes.history
-}
+// AppRenderer.propTypes = {
+//   history: ReactRouterPropTypes.history
+// }
 
+/*
 ReactDOM.render(
   <RISPProvider
     onBlur={() => cursor.enableHandler()}
@@ -73,5 +74,5 @@ ReactDOM.render(
   </RISPProvider>,
   document.getElementById('root')
 )
-
-module && module.hot && module.hot.accept()
+*/
+ReactDOM.render(<div>TODO: Implement</div>, document.getElementById('app'))
