811c6b5d579d0e15b7d78542df11cdd2e793dae7
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2023-07-24 18:10:01 +0300

    Bare bone middleware example.

--- apps/bookkeeper-ui/vite.config.ts
+++ apps/bookkeeper-ui/vite.config.ts
@@ -1,13 +1,32 @@
+/** @type {import('vite').UserConfig} */
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import 'dotenv/config'
+
+const myPlugin = () => ({
+  name: 'configure-server',
+  configureServer(server) {
+    return () => {
+      server.middlewares.use((req, res, next) => {
+        if (req.originalUrl.startsWith('/internal/plugins')) {
+          res.setHeader('Content-Type', 'application/json')
+          res.end('[]')
+          return
+        }
+        next()
+      })
+    }
+  },
+})
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
+    appType: 'spa',
     plugins: [
+      myPlugin(),
       react({
         babel: {
           plugins: [
