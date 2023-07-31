1b4d7ba8a46923b97e0527f8669fefc023e32a24
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2023-07-31 13:42:46 +0300

    Remove import of jest stuff.

--- packages/tasenor-common-node/src/testing/UnitTester.ts
+++ packages/tasenor-common-node/src/testing/UnitTester.ts
@@ -1,4 +1,3 @@
-import { expect } from '@jest/globals'
 import { AccountAddress, AccountNumber, AdditionalTransferInfo, AssetTransfer, StockValueData, Transaction, TransactionDescription, TransactionLine, ImportStateText, ImportConfig } from '@dataplug/tasenor-common'
 import { TransactionImportHandler, AskUI, Process } from '..'
 import { getTestHandler } from './test-handlers'
