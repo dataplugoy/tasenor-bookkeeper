5a388149a94f9e7ac86fbb198cd2c09d7dc74a68
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2023-08-22 11:53:58 +0300

    QA path resolver.

--- packages/tasenor-testing/robot/libraries/directories.py
+++ packages/tasenor-testing/robot/libraries/directories.py
@@ -5,3 +5,10 @@ def data_files_directory():
     Return directory containing data files for testing.
     """
     return os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'data', 'files'))
+
+
+def qa_file(test, name):
+    """
+    Locate a file in QA repo.
+    """
+    return os.path.realpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', '..', '..', '..', 'tests', test, 'data', 'files', name))
