67e64ec664e5e7e17b3549483273fc5a1e202e36
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2023-08-25 17:42:24 +0300

    Dropped investing plugins.

--- packages/tasenor-common-plugins/data/FinnishInvestmentCompany.mjs
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import { readFile, saveFile, trimCRLF } from './lib/utils.mjs'
-
-const tsv = trimCRLF(readFile('FinnishInvestmentCompany - fi-EUR.tsv'))
-saveFile('FinnishInvestmentCompany', 'backend/fi-EUR.tsv', tsv)
