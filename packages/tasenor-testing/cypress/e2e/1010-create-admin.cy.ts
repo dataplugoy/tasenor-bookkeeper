ac5d3c007313541d20a43e8b3c5478e454cee8e1
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2024-01-28 19:29:58 +0200

    ABA-602: Sketch admin creation test.

--- packages/tasenor-testing/cypress/e2e/1010-create-admin.cy.ts
+++ packages/tasenor-testing/cypress/e2e/1010-create-admin.cy.ts
@@ -1,7 +1,14 @@
 /// <reference types="../support/index.d.ts" />
 
 describe('Initialize admin', () => {
-  it('Create admin user', () => {
+  it('Create admin user, if not yet done', () => {
     cy.visit('/')
+    cy.get('.MainArea').then($page => {
+       if ($page.html().includes('Please register an admin user')) {
+
+       } else {
+        // Verify login.
+       }
+    })
   })
 })
