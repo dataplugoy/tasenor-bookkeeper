3204fb5e7c96989a224b2c1b9cb3f8e7a47ee10a
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2024-01-29 19:38:02 +0200

    ABA-602: Create QA user test.

---
+++ packages/tasenor-testing/cypress/e2e/1035-create-qa-user.cy.ts
@@ -0,0 +1,19 @@
+/// <reference types="../support/index.d.ts" />
+
+describe('Initialize user', () => {
+  it('Create new user', () => {
+
+    cy.fixture('ci.json').then((config) => {
+      cy.adminLogin()
+      cy.goto('Admin', 'Users', 'create-user')
+      cy.form({
+        'Full Name': 'QA',
+        Password: config.QA_PASSWORD,
+        'Password Again': config.QA_PASSWORD,
+        Email: config.QA_USER,
+      })
+      cy.button('Submit').click()
+      cy.messages().should('contain.text', 'User created successfully.')
+    })
+  })
+})
