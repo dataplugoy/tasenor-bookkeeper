7eff27ef74bea3f36f5f659da679834efc23dd15
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2024-02-02 16:24:58 +0200

    ABA-628: Make server launcher as library function.

--- apps/bookkeeper-api/src/server.ts
+++ apps/bookkeeper-api/src/server.ts
@@ -1,8 +1,8 @@
 import path from 'path'
-import express from 'express'
+import express, { Express } from 'express'
 import config from './config'
-import { log, Url, DirectoryPath, setServerRoot, error, waitPromise } from '@tasenor/common'
-import { vault, tasenorInitialStack, tasenorFinalStack, plugins, killPortUser } from '@tasenor/common-node'
+import { log, Url, DirectoryPath, setServerRoot } from '@tasenor/common'
+import { vault, tasenorInitialStack, tasenorFinalStack, plugins, listen } from '@tasenor/common-node'
 import db from './lib/db'
 import server from './lib/server'
 import catalog from './lib/catalog'
@@ -10,49 +10,31 @@ import cron from './lib/cron'
 import pkg from '../package.json'
 import routes from './routes'
 
-const app = express()
+async function main(listener) {
+  server.register()
 
-/**
- * Try to establish a listener for the server port.
- */
-async function listener(): Promise<boolean> {
-  return new Promise((resolve) => {
-    log('Setting up listener...')
+  log('Checking default plugin repos...')
+  const src = path.join(__dirname, '..', '..', '..') as DirectoryPath
+  plugins.setConfig('INITIAL_PLUGIN_REPOS', process.env.INITIAL_PLUGIN_REPOS || '')
+  const changes = await plugins.fetchRepositories(src)
 
-    const listener = app.listen(config.PORT,
-    ).on('error', (err) => {
-      error('Launching failed:', err + '')
-      if ('code' in err && err.code === 'EADDRINUSE') {
-        error('Trying to kill existing process...')
-        killPortUser(config.PORT)
-      }
-      resolve(false)
-    }).on('listening', async () => {
-      server.register()
+  // Rebuild plugin index if changes.
+  if (changes) {
+    await plugins.updatePluginList()
+  }
 
-      log('Checking default plugin repos...')
-      const src = path.join(__dirname, '..', '..', '..') as DirectoryPath
-      plugins.setConfig('INITIAL_PLUGIN_REPOS', process.env.INITIAL_PLUGIN_REPOS || '')
-      const changes = await plugins.fetchRepositories(src)
+  await catalog.reload()
 
-      // Rebuild plugin index if changes.
-      if (changes) {
-        await plugins.updatePluginList()
-      }
+  cron.initialize()
+  log('Bookkeeper back-end server listening on port ' + config.PORT)
 
-      await catalog.reload()
-
-      cron.initialize()
-      log('Bookkeeper back-end server listening on port ' + config.PORT)
-
-      server.setServer(listener)
-      resolve(true)
-    })
-  })
+  server.setServer(listener)
 }
 
-async function main() {
+async function launch() {
   log(`Starting server v${pkg.version}`)
+
+  const app: Express = express()
   setServerRoot(path.join(__dirname, '..'))
   await vault.initialize()
   await db.migrate()
@@ -60,13 +42,9 @@ async function main() {
   app.use(tasenorInitialStack({ origin: process.env.UI_ORIGIN_URL as Url }))
   app.use('/', routes)
   app.use(tasenorFinalStack())
-
-  while (true) {
-    if (await listener()) break
-    await waitPromise(2000)
-  }
+  listen(app, config.PORT, main)
 }
 
 (async () => {
-  main()
+  launch()
 })()
---
+++ packages/tasenor-common-node/src/server/server.ts
@@ -0,0 +1,36 @@
+import { Server } from 'http'
+import { Express } from 'express'
+import { log, error, waitPromise } from '@tasenor/common'
+import { killPortUser } from '../system'
+
+/**
+ * Try to establish a listener for the server port.
+ */
+async function listener(app: Express, port: number, main: (s: Server) => void): Promise<boolean> {
+  return new Promise((resolve) => {
+    log('Setting up listener...')
+
+    const listener = app.listen(port,
+    ).on('error', (err) => {
+      error('Launching failed:', err + '')
+      if ('code' in err && err.code === 'EADDRINUSE') {
+        error('Trying to kill existing process...')
+        killPortUser(port)
+      }
+      resolve(false)
+    }).on('listening', async () => {
+      main(listener)
+      resolve(true)
+    })
+  })
+}
+
+/**
+ * Launch the listener for a server.
+ */
+export async function listen(app: Express, port: number | string, main: (s: Server) => void) {
+  while (true) {
+    if (await listener(app, parseInt(`${port}`), main)) break
+    await waitPromise(2000)
+  }
+}
