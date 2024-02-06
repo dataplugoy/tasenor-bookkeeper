d3764948f4d1d20ea67e8e25e7c3a1a0754bcd1f
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2024-02-06 16:30:00 +0200

    ABA-623: Convert menu to functional component.

--- packages/tasenor-common/src/types/bookkeeper/cursor.ts
+++ packages/tasenor-common/src/types/bookkeeper/cursor.ts
@@ -1,5 +1,17 @@
+import Opaque from "ts-opaque"
+
 /**
- * A keyboard handler
+ * A string definition of a key press as in `keydown` event listener.
+ */
+export type KeyboardShortcut = Opaque<string, 'KeyboardShortcut'>
+
+/**
+ * A handler function collection for keyboard shortcuts.
+ */
+export type ShortcutHandlers = Record<string, (Cursor, KeyboardShortcut) => undefined | { preventDefault: true }>
+
+/**
+ * A keyboard handler.
  */
 export declare class Cursor {
   disableHandler(): void
@@ -7,4 +19,6 @@ export declare class Cursor {
   handle(key: string): void
   addModal(name: string, hooks: Record<string, (cursor: Cursor, key: string) => void>)
   removeModal(name: string)
+  resetSelected(): void
+  registerMenu(handlers: ShortcutHandlers)
 }
