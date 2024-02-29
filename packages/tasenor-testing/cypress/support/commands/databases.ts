b09a245062dfc1f3a55c7249bdf40b91023c34c4
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2024-02-29 10:49:37 +0200

    ABA-604: Add some type definitions.

--- packages/tasenor-testing/cypress/support/commands/databases.ts
+++ packages/tasenor-testing/cypress/support/commands/databases.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+/// <reference types="cypress" />
+
 export {}
 
 /**
--- packages/tasenor-testing/cypress/support/commands/inspection.ts
+++ packages/tasenor-testing/cypress/support/commands/inspection.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+/// <reference types="cypress" />
 /**
  * Find the currently selected database name.
  */
