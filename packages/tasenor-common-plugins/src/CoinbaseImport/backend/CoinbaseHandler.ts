ab6c96ede43d6f78f51f31138137fa7ed6fddae1
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2024-03-11 19:50:22 +0200

    ABA-679: Handle 3rd format for Coinbase.

--- packages/tasenor-common-plugins/src/CoinbaseImport/backend/CoinbaseHandler.ts
+++ packages/tasenor-common-plugins/src/CoinbaseImport/backend/CoinbaseHandler.ts
@@ -40,39 +40,53 @@ export class CoinbaseHandler extends TransactionImportHandler {
     if (file.firstLineMatch(/"You can use this transaction report/)) {
       return 2
     }
+    if (file.firstLineMatch(/^$/) && file.secondLineMatch(/^Transactions$/)) {
+      return 3
+    }
 
     return 0
   }
 
   async init(files: ProcessFile[]): Promise<ProcessFile[]> {
-    if (this.version === 2) {
+    if (this.version === 1) {
+      return files
+    }
+    if (this.version === 2 || this.version === 3) {
       // Clean up trash from the beginning.
       for (const file of files) {
         const data = file.decode()
-        const lines = data.split('\n').splice(7)
+        const lines = data.split('\n').splice(this.version === 2 ? 7 : 3)
         if (!/^Timestamp,Transaction Type,Asset,Quantity Transacted,Spot Price Currency,Spot Price at Transaction,Subtotal,Total/.test(lines[0])) {
           throw new Error('Coinbase parser is not able to understand the format.')
         }
         file.set(lines.join('\n'))
       }
+      return files
     }
-    return files
+
+    throw new Error(`Coinbase import handler does not implement init() for version ${this.version}.`)
   }
 
   segmentId(line: TextFileLine): SegmentId | typeof NO_SEGMENT {
-    if (this.version === 2) {
+    if (this.version === 2 || this.version === 3) {
       return super.segmentId(line)
     }
-    if (['deposit', 'withdrawal'].includes(line.columns.type)) {
-      return line.columns['transfer id']
+    if (this.version === 1) {
+      if (['deposit', 'withdrawal'].includes(line.columns.type)) {
+        return line.columns['transfer id']
+      }
+      return line.columns['trade id']
     }
-    return line.columns['trade id']
+    throw new Error(`Coinbase import handler does not implement segmentId() for version ${this.version}.`)
   }
 
   time(line: TextFileLine): Date | undefined {
-    if (this.version === 2) {
+    if (this.version === 2 || this.version === 3) {
       return line.columns.Timestamp ? new Date(line.columns.Timestamp) : undefined
     }
-    return line.columns.time ? new Date(line.columns.time) : undefined
+    if (this.version === 1) {
+      return line.columns.time ? new Date(line.columns.time) : undefined
+    }
+    throw new Error(`Coinbase import handler does not implement time() for version ${this.version}.`)
   }
 }
