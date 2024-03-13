7d5b4955436d03aa84352070014660699f11c6df
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2024-03-13 17:50:49 +0200

    ABA-685: Refactor import tool panel.

---
+++ apps/bookkeeper/src/Components/ImportToolPanel.tsx
@@ -0,0 +1,166 @@
+import React, { Component, useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
+import { observer } from 'mobx-react'
+import { withTranslation, Trans, useTranslation } from 'react-i18next'
+import Store from '../Stores/Store'
+import { Dialog, IconSpacer, IconButton, Title, FileUploader, useNav } from '@tasenor/common-ui'
+import ImporterConfigEditor from './ImporterConfigEditor'
+import { TextField, MenuItem, FormControl, Box } from '@mui/material'
+import ImporterModel from '../Models/ImporterModel'
+import Catalog from '../Stores/Catalog'
+import { ID, haveCursor, haveSettings } from '@tasenor/common'
+import i18n from '../i18n'
+import withRouter from '../Hooks/withRouter'
+import withStore from '../Hooks/withStore'
+import withCatalog from '../Hooks/withCatalog'
+import { useParams } from 'react-router-dom'
+
+interface ImportToolPanelProps {
+  catalog: Catalog
+  store: Store
+}
+
+const ImportToolPanel = observer(withStore(withCatalog((props: ImportToolPanelProps): JSX.Element => {
+
+  const { store, catalog } = props
+  const { importerId } = useParams()
+  const { t } = useTranslation()
+  const canImport = Object.keys(catalog.getImportOptions()).length > 0
+  const nav = useNav()
+
+  const [showCreateImportDialog, setShowCreateImportDialog] = useState(false)
+  const [showEdiSettingstDialog, setShowEdiSettingstDialog] = useState(false)
+  const [changed, setChanged] = useState(false)
+  const [handler, setHandler] = useState('')
+  const [name, setName] = useState('')
+
+  useEffect(() => {
+    const cursor = haveCursor()
+
+    cursor.registerTools({
+      keyIconA: () => {
+        const options = catalog.getImportOptions()
+        if (Object.keys(options).length === 0) {
+          return
+        }
+        setShowCreateImportDialog(true)
+        return { preventDefault: true, changed: false }
+      },
+      keyIconS: () => {
+        if (importerId) {
+          setShowEdiSettingstDialog(!showEdiSettingstDialog)
+        }
+        return { preventDefault: true }
+      }
+
+    })
+
+    return () => {
+      cursor.registerTools(null)
+    }
+  }, [])
+
+  if (!store.isLoggedIn()) {
+    return <></>
+  }
+
+  const options = catalog.getImportOptions()
+
+  const onUpload = async function(files) {
+    if (!importerId) {
+      store.addError('Arbitrary file upload not yet implemented. Please create and select an importer.')
+      return
+    }
+
+    const firstDate = store.period ? store.period.start_date : null
+    const lastDate = store.period ? store.period.end_date : null
+    const data = await store.uploadImportFiles(importerId, { files, firstDate, lastDate })
+
+    if (data) {
+      await store.fetchBalances()
+      nav.go({ processId: data.processId, step: data.step })
+    } else {
+      store.addError(t('Importing a file failed.'))
+    }
+  }
+
+  const onCreateImport = async function() {
+    const settings = haveSettings()
+    const importer = new ImporterModel(store, {
+      name,
+      config: {
+        currency: settings.get('currency'),
+        handlers: [handler],
+        language: i18n.language
+      }
+    })
+    await importer.save()
+    setShowCreateImportDialog(false)
+    setChanged(false)
+    setName('')
+
+    store.addMessage(t('New importer created.'))
+
+    nav.go({ side: `${importer.id}` })
+  }
+
+  const onFinishEdit = async function() {
+    setShowEdiSettingstDialog(false)
+    store.fetchImporters(store.db)
+  }
+
+  return <Box className="ToolPanel ImportToolPanel">
+    <Title><Trans>Data Transfer Center</Trans></Title>
+    <IconButton id="CreateImport" disabled={!canImport} shortcut="A" pressKey="IconA" title="new-import" icon="new"/>
+    <IconButton id="ConfigureImport" disabled={!importerId} shortcut="S" pressKey="IconS" title="import-settings" icon="settings"/>
+    <IconSpacer />
+    <FileUploader color="primary" disabled={!canImport} variant="contained" onUpload={files => onUpload(files)}/>
+
+    <Dialog
+      className="CreateImport"
+      wider
+      title={<Trans>Create New Import</Trans>}
+      isVisible={showCreateImportDialog}
+      onClose={() => setShowCreateImportDialog(false)}
+      onConfirm={() => onCreateImport()}
+      isValid={() => !!handler && !!name}
+    >
+    <FormControl fullWidth>
+      <TextField
+        select
+        name="handler"
+        value={handler}
+        label={<Trans>Select plugin for handling the import</Trans>}
+        error={changed && !handler}
+        helperText={changed && !handler ? t('Plugin is required.') : ''}
+        onChange={(event) => {
+          setChanged(true)
+          setHandler(event.target.value)
+        }}
+      >
+        <MenuItem value=""></MenuItem>
+        {Object.entries(options).map(([code, title]) => <MenuItem key={code} value={code}><Trans>{title}</Trans></MenuItem>)}
+      </TextField>
+    </FormControl>
+    <FormControl fullWidth>
+      <TextField
+        name="name"
+        fullWidth
+        error={changed && !name}
+        helperText={changed && !name ? t('Name is required.') : ''}
+        label={<Trans>Give name for the import process</Trans>}
+        value={name}
+        onChange={(event) => {
+          setChanged(true)
+          setName(event.target.value)
+        }}
+      />
+    </FormControl>
+    </Dialog>
+
+    <ImporterConfigEditor visible={showEdiSettingstDialog} importerId={parseInt(importerId || '') as ID} onClose={() => onFinishEdit()} />
+
+  </Box>
+})))
+
+export default ImportToolPanel
