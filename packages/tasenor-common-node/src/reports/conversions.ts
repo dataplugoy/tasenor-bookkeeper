e2912ec4621928b04dc0b8ae4f0dbbc29c9bb4fd
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2024-07-03 15:08:14 +0300

    No stringifying.

--- packages/tasenor-common-node/src/reports/conversions.ts
+++ packages/tasenor-common-node/src/reports/conversions.ts
@@ -14,7 +14,7 @@ export function data2csv(report, options: ReportQueryParams) {
   const render = {
     id: (column, entry) => entry.id,
     name: (column, entry) => entry.name === undefined ? '' : `${entry.isAccount ? entry.number + ' ' : ''}${entry.name}`,
-    text: (column, entry) => `${entry.values[column.name]}`,
+    text: (column, entry) => entry.values[column.name],
     numeric: (column, entry) => (entry.values &&
       !entry.hideTotal &&
       entry.values[column.name] !== '' &&
