8025dafae948fc684f0565e6c4fe8478adfbca03
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2025-10-19 20:22:41 +0300

    Work on trade fixes.

--- packages/tasenor-common-plugins/src/CoinbaseImport/backend/CoinbaseHandler.ts
+++ packages/tasenor-common-plugins/src/CoinbaseImport/backend/CoinbaseHandler.ts
@@ -46,11 +46,11 @@ export class CoinbaseHandler extends TransactionImportHandler {
     if (file.firstLineMatch(/^$/) && file.secondLineMatch(/^Transactions$/)) {
       const data = file.decode()
       const lines = data.split('\n').splice(3)
-      if (!/^Timestamp,Transaction Type,Asset,Quantity Transacted,Spot Price Currency,Spot Price at Transaction,Subtotal,Total/.test(lines[0])) {
-        return 3
-      }
       if (!/^ID,Timestamp,Transaction Type,Asset,Quantity Transacted,Price Currency,Price at Transaction,Subtotal,Total (inclusive of fees and\/or spread),Fees and\/or Spread,Notes/.test(lines[0])) {
         return 4
+      }
+      if (!/^Timestamp,Transaction Type,Asset,Quantity Transacted,Spot Price Currency,Spot Price at Transaction,Subtotal,Total/.test(lines[0])) {
+        return 3
       }
 
       throw new Error('Coinbase parser is not able to understand the column format.')
@@ -104,6 +104,39 @@ export class CoinbaseHandler extends TransactionImportHandler {
     if (columns.type === 'Convert') {
       const match = /Converted ([0-9.]+) (\w+) to ([0-9.]+) (\w+)/.exec(columns.Notes as string)
       if (match) {
+        columns.giveAmount = parseFloat(match[1])
+        columns.giveAsset = match[2]
+        columns.takeAmount = parseFloat(match[3])
+        columns.takeAsset = match[4]
+      }
+    }
+    if (this.version === 4) {
+      if (columns.type === 'Advanced Trade Buy') {
+        //        1          2       3                 4      5   6              7   8
+        // Bought 0.33694052 ETH for 0.012835268969159 BTC on ETH-BTC at 0.03781 BTC/ETH
+        const match = /Bought ([0-9.]+) ([A-Z]+) for ([0-9.]+) ([A-Z]+) on ([A-Z]+)-([A-Z]+) at [0-9.]+ ([A-Z]+)\/([A-Z]+)/.exec(`${columns.Notes}`)
+        if (!match) {
+          throw new Error(`Unexpected format for notes: "${columns.Notes}".`)
+        }
+        if (match[2] !== match[5] || match[5] !== match[8] || match[4] !== match[6] || match[6] !== match[7]) {
+          throw new Error(`Coin currencies did not match as expected: "${columns.Notes}"`)
+        }
+        columns.type = 'Advanced Convert'
+        columns.takeAmount = parseFloat(match[1])
+        columns.takeAsset = match[2]
+        columns.giveAmount = parseFloat(match[3])
+        columns.giveAsset = match[4]
+      } else if (columns.type === 'Advanced Trade Sell') {
+        //      1          2       3                  4      5   6              7   8
+        // Sold 0.41459303 ETH for 0.0128917806326757 BTC on ETH-BTC at 0.03133 BTC/ETH
+        const match = /Sold ([0-9.]+) ([A-Z]+) for ([0-9.]+) ([A-Z]+) on ([A-Z]+)-([A-Z]+) at [0-9.]+ ([A-Z]+)\/([A-Z]+)/.exec(`${columns.Notes}`)
+        if (!match) {
+          throw new Error(`Unexpected format for notes: "${columns.Notes}".`)
+        }
+        if (match[2] !== match[5] || match[5] !== match[8] || match[4] !== match[6] || match[6] !== match[7]) {
+          throw new Error(`Coin currencies did not match as expected: "${columns.Notes}"`)
+        }
+        columns.type = 'Advanced Convert'
         columns.giveAmount = parseFloat(match[1])
         columns.giveAsset = match[2]
         columns.takeAmount = parseFloat(match[3])
