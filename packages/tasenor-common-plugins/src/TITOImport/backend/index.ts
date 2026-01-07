5b95d244d1f7a50d748662dc96055f9517c7352d
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2026-01-07 14:24:21 +0200

    Version update v1.17.9

--- packages/tasenor-common-plugins/src/TITOImport/backend/index.ts
+++ packages/tasenor-common-plugins/src/TITOImport/backend/index.ts
@@ -9,9 +9,9 @@ class TITOImportPlugin extends ImportPlugin {
 
     this.code = 'TITOImport' as PluginCode
     this.title = 'Import for TITO'
-    this.version = '1.0.30' as Version
+    this.version = '1.0.31' as Version
     this.icon = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M5 6.99h3V14h2V6.99h3L9 3zM14 10v7.01h-3L15 21l4-3.99h-3V10z"/></svg>'
-    this.releaseDate = '2023-07-03'
+    this.releaseDate = '2026-01-07'
     this.use = 'backend'
     this.type = 'import'
     this.description = 'Import plugin for importing transaction data from old fixed length field TITO format.'
