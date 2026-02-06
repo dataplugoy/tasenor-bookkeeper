50d20fc1ec3557404b6cb83695b49fe7c82b7f61
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2026-02-06 16:35:22 +0200

    Income statement report.

---
+++ packages/tasenor-common-plugins/data/EstonianIncomeStatementReport.mjs
@@ -0,0 +1,10 @@
+#!/usr/bin/env -S npx tsx
+import commonNode from '@tasenor/common-node'
+const { readFile, saveText, trimCRLF } = commonNode.dataUtils
+
+let tsv
+tsv = trimCRLF(readFile('EstonianIncomeStatementReport - income-statement-fi.tsv'))
+saveText('EstonianIncomeStatementReport', 'income-statement-fi.tsv', tsv)
+
+tsv = trimCRLF(readFile('EstonianIncomeStatementReport - income-statement-detailed-fi.tsv'))
+saveText('EstonianIncomeStatementReport', 'income-statement-detailed-fi.tsv', tsv)
