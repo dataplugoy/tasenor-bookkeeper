a857736de9777c4cff9bfdd1ab8525bd41b8cf9c
Tommi Ronkainen <tommi.ronkainen@gmail.com>
2026-02-09 17:24:52 +0200

    English versions of estonian reports.

--- packages/tasenor-common-plugins/data/EstonianBalanceSheetReport.mjs
+++ packages/tasenor-common-plugins/data/EstonianBalanceSheetReport.mjs
@@ -4,6 +4,10 @@ const { readFile, saveText, trimCRLF } = commonNode.dataUtils
 
 let tsv = trimCRLF(readFile('EstonianBalanceSheetReport - balance-sheet-fi.tsv'))
 saveText('EstonianBalanceSheetReport', 'balance-sheet-fi.tsv', tsv)
+tsv = trimCRLF(readFile('EstonianBalanceSheetReport - balance-sheet-en.tsv'))
+saveText('EstonianBalanceSheetReport', 'balance-sheet-en.tsv', tsv)
 
 tsv = trimCRLF(readFile('EstonianBalanceSheetReport - balance-sheet-detailed-fi.tsv'))
 saveText('EstonianBalanceSheetReport', 'balance-sheet-detailed-fi.tsv', tsv)
+tsv = trimCRLF(readFile('EstonianBalanceSheetReport - balance-sheet-detailed-en.tsv'))
+saveText('EstonianBalanceSheetReport', 'balance-sheet-detailed-en.tsv', tsv)
--- packages/tasenor-common-plugins/data/EstonianIncomeStatementReport.mjs
+++ packages/tasenor-common-plugins/data/EstonianIncomeStatementReport.mjs
@@ -5,6 +5,10 @@ const { readFile, saveText, trimCRLF } = commonNode.dataUtils
 let tsv
 tsv = trimCRLF(readFile('EstonianIncomeStatementReport - income-statement-fi.tsv'))
 saveText('EstonianIncomeStatementReport', 'income-statement-fi.tsv', tsv)
+tsv = trimCRLF(readFile('EstonianIncomeStatementReport - income-statement-en.tsv'))
+saveText('EstonianIncomeStatementReport', 'income-statement-en.tsv', tsv)
 
 tsv = trimCRLF(readFile('EstonianIncomeStatementReport - income-statement-detailed-fi.tsv'))
 saveText('EstonianIncomeStatementReport', 'income-statement-detailed-fi.tsv', tsv)
+tsv = trimCRLF(readFile('EstonianIncomeStatementReport - income-statement-detailed-en.tsv'))
+saveText('EstonianIncomeStatementReport', 'income-statement-detailed-en.tsv', tsv)
